2b5f6c1c1ed3057ddf25f2050995f6d75129a6b2
Parajux <30836949+Parajux@users.noreply.github.com>
2024-05-27 19:41:37 +0000

    Motified

--- src/Ch03/03_02b/CodeDemo.cpp
+++ src/Ch03/03_02b/CodeDemo.cpp
@@ -8,8 +8,23 @@
 enum class cow_purpose {dairy, meat, hide, pet};
 
 class cow{
-    /* public:  //class is privte if not declared as public */
-    private:
+ public:  //class is privte if not declared as public 
+    cow(std::string name_i, int age_i, cow_purpose purpose_i){
+        name = name_i;
+        age = age_i;
+        purpose = purpose_i;
+    }
+    std::string get_name() const{
+        return name;
+    }
+    int get_age()const {
+        return age;
+    }
+    cow_purpose get_purpose() const{
+        return purpose;
+    }
+
+
     std::string name;
     int age;
     cow_purpose purpose;
